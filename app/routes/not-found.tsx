3ef797ba59e33e8ed2784da2ba0f9b4bef87d2cb
Ashuksu <ashuksu@gmail.com>
2025-07-25 17:25:22 +0200

    master - Added NotFoundPage (404) component and update routes configuration

---
+++ app/routes/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <main className="pt-16 p-4 container mx-auto">
+            <h1>404</h1>
+            <p>No pages found</p>
+        </main>
+    );
+}
