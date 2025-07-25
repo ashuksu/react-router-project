eea0237dd16244386f10170cd3153d47bd60e61a
Ashuksu <ashuksu@gmail.com>
2025-07-25 21:51:31 +0200

    master - Updated routes to replace NotFoundPage with Error and change About page to History

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import {index, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
     index("pages/home.tsx"),
-    route("about", "pages/about.tsx"),
-    route("*", "pages/not-found.tsx")
+    route("about", "pages/history.tsx"),
+    route("*", "pages/error.tsx")
 ] satisfies RouteConfig;
