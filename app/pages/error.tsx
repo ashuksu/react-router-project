129cf55dbd7cd054a46613686b559653aa00d2a5
Ashuksu <ashuksu@gmail.com>
2025-07-25 21:51:20 +0200

    master - Rename NotFoundPage to Error and enhance layout with Main and Container components

---
+++ app/pages/error.tsx
@@ -0,0 +1,13 @@
+import {Main} from "~/components/Main";
+import {Container} from "~/components/Container";
+
+export default function Error() {
+    return (
+        <Main>
+            <Container className="flex min-h-80 items-center justify-center flex-col text-center py-6">
+                <h1 className="text-center text-2xl font-bold mb-4">404</h1>
+                <p>No pages found</p>
+            </Container>
+        </Main>
+    );
+}
--- app/pages/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFoundPage() {
-    return (
-        <main className="pt-16 p-4 container mx-auto">
-            <h1>404</h1>
-            <p>No pages found</p>
-        </main>
-    );
-}
